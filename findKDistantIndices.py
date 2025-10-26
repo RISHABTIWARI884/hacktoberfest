a3c7046a8d199ad54bce6fcce5816e8e71381be5
RISHAB TIWARI <111065846+RISHABTIWARI884@users.noreply.github.com>
2025-10-26 23:47:54 +0530

    Implement method to find k-distant indices.py

---
+++ findKDistantIndices.py
@@ -0,0 +1,13 @@
+from typing import List
+
+class Solution:
+    def findKDistantIndices(self, nums: List[int], key: int, k: int) -> List[int]:
+        index = []
+        n = len(nums)
+        
+        for i in range(n):
+            for j in range(n):
+                if nums[j] == key and abs(i - j) <= k:
+                    index.append(i)
+                    break  # no need to check other j's
+        return index
